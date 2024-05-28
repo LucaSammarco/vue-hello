215742d79019a8629bcd0ff604da97008d3b3c6b
Il tuo nome <la_tua_email@example.com>
2024-05-28 16:29:03 +0200

    added input box to change h1 and image url

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Ciao Gatto",
-            // car: "bla",
-            
+            message: "",
+                    
             immagine: "https://www.ilverdemondo.it/public/blog/thumbs/gatto-del-bengala-carattere-alimentazione-cure-it-000.jpg"
 
 
